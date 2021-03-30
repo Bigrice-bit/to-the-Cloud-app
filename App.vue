04f53d129b29fb2eae91b75856cfaca43f359dc0
Bigrice-bit <1151072975@qq.com>
2021-03-30 15:37:57 +0800

    欢迎页、登录、注册等初步完成

---
+++ App.vue
@@ -0,0 +1,38 @@
+<script>
+	export default {
+		onLaunch: function() {  
+		    uni.getSystemInfo({  
+		        success:function(e){  
+		            Vue.prototype.statusBar = e.statusBarHeight  
+		            // #ifndef MP  
+		            if(e.platform == 'android') {  
+		                Vue.prototype.customBar = e.statusBarHeight + 50  
+		            }else {  
+		                Vue.prototype.customBar = e.statusBarHeight + 45  
+		            }  
+		            // #endif  
+		
+		            // #ifdef MP-WEIXIN  
+		            let custom = wx.getMenuButtonBoundingClientRect()  
+		            Vue.prototype.customBar = custom.bottom + custom.top - e.statusBarHeight  
+		            // #endif  
+		
+		            // #ifdef MP-ALIPAY  
+		            Vue.prototype.customBar = e.statusBarHeight + e.titleBarHeight  
+		            // #endif  
+		        }  
+		    })  
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+</style>""
---
+++ pages/index/welcome.vue
@@ -0,0 +1,89 @@
+<template>
+	<view class="main">
+		<code-elf-guide v-if="guidePages"></code-elf-guide>
+	</view>
+<!-- 	<view class="wrap">
+			<u-swiper :list="list"></u-swiper>
+		</view> -->
+</template>
+
+<script>
+	import codeElfGuide from '@/components/code-elf-guide/code-elf-guide.vue'
+	export default {
+        components: {
+            codeElfGuide
+        },
+		data() {
+			return {
+				guidePages:true,
+				list: [
+					'../../static/1.png',
+					'../../static/2.png',
+					'../../static/3.png',
+					'../../static/3.png'
+				]
+			}
+		},
+		onLoad(){
+				// var _me = this;           
+				// uni.getSystemInfo({//获取手机屏幕高度信息，让swiper的高度和手机屏幕一样高                
+				// 	success: function(res) {                    
+				// 	console.log(res.model);                    
+				// 	console.log(res.pixelRatio);                    
+				// 	console.log(res.windowWidth);                    
+				// 	console.log("高度值"+res.windowHeight);//这里是手机屏幕高度                    
+				// 	console.log(res.language);                   
+				// 	console.log(res.version);                    
+				// 	console.log(res.platform);                    
+				// 	_me.windowHeight = res.windowHeight + 'px';                    
+				// 	console.log('手机屏幕高度为' + _me.windowHeight);               
+				// 			}            
+				// });        
+			this.loadExecution()
+		},
+		methods: {
+			loadExecution: function(){
+				/**
+				 * 获取本地存储中launchFlag的值
+				 * 若存在，说明不是首次启动，直接进入首页；  先进入首页 之后再根据是否过期判断跳转首页或登录页
+				 * 若不存在，说明是首次启动，进入引导页；
+				 */
+				try {
+					// 获取本地存储中launchFlag标识
+				    const value = uni.getStorageSync('launchFlag');
+				    if (value) {
+						console.log(value);
+						// launchFlag=true直接跳转到首页
+						uni.redirectTo({
+							url: '/pages/login/login'
+						});
+				    } else {
+						// launchFlag!=true显示引导页
+				        this.guidePages = true
+				    }
+				} catch(e) { 
+					// error 
+					uni.setStorage({ 
+						key: 'launchFlag', 
+						data: true, 
+						success: function () {
+							console.log('error时存储launchFlag');
+						} 
+					}); 
+					this.guidePages = true
+				}
+				return;
+				// uni.switchTab({
+				//     url: '/pages/tabBar/component/component'
+				// });
+			}
+		}
+	}
+</script>
+
+<style>
+	page,.main{
+		width: 100%;
+		height: 100%;
+	}
+</style>
