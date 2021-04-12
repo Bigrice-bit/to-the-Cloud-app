e67650786624277f7e2852683a5c89ec57cefc45
Bigrice-bit <71315075+Bigrice-bit@users.noreply.github.com>
2021-04-12 23:35:12 +0800

    班课列表与创建班课

---
+++ pages/class/success-create.vue
@@ -0,0 +1,66 @@
+<template>
+	<view>
+	<u-navbar title-color="#fff" back-icon-color="#ffffff"
+		:is-fixed="isFixed" :is-back="isBack" 
+		:background="background" 
+		:back-text-style="{color: '#fff'}" :title="title" 
+		:back-icon-name="backIconName" :back-text="backText"
+		@click="newcreate"
+	>
+	</u-navbar>
+	<image class="swiper-item-img" src="../../static/success.png" mode="widthFix"></image>
+	<button class="sss" type="default">返回</button></view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+			title: '创建成功',
+			backText: '返回',
+			backIconName: 'nav-back',
+			right: false,
+			showAction: false,
+			rightSlot: false,
+			useSlot: false,
+			background: {
+				'background-image': 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'
+			},
+			isBack: false,
+			search: false,
+			custom: false,
+			isFixed: true,
+			keyword: '',
+			// #ifdef MP
+			slotRight: false,
+			// #endif
+			// #ifndef MP
+			slotRight: true,
+			// #endificon: 'setting',
+			arrow: true,
+			label: '签到  提问 回复',
+			rightSlot: true,
+			checked: false
+			}
+		}
+	}
+</script>
+
+<style>
+	.swiper-item-img {
+		width: 100%;
+		height: 100%;
+		margin: 0 auto;
+	},
+	.sss{
+		position: absolute;
+		height: 80upx;
+		line-height: 80upx;
+		padding: 0 40upx;
+		border-radius: 30upx;
+		font-size: 35upx;
+		color: #454343;
+		border: 1px solid #454343;
+		z-index: 999;
+	}
+</style>
--- pages/index/class.vue
+++ pages/index/class.vue
@@ -1,444 +1,261 @@
 <template>
-	<view class="content">
-		<view class="wrap">
-			<view class="u-tabs-box">
-				<u-tabs-swiper activeColor="#43c191" name="cate_name" count="cate_count" ref="tabs" :list="list"
-					bar-height="3" bar-width="200" :current="current" @change="change" :is-scroll="false"
-					swiperWidth="750"></u-tabs-swiper>
+	<view>
+		<u-navbar title-color="#fff" back-icon-color="#ffffff" 
+			:is-fixed="isFixed" :is-back="isBack" 
+			:background="background" 
+			:back-text-style="{color: '#fff'}" :title="title" 
+			:back-icon-name="backIconName" :back-text="backText"
+			@click="newcreate"
+		>
+			<view class="navbar-right" slot="right" v-if="slotRight">
+				<view class="message-box right-item">
+					<u-icon name="plus" size="38" @click="newcreate"></u-icon>
+					<u-badge count="0" size="mini" :offset="[-15, -15]"></u-badge>
+				</view>
 			</view>
-			<swiper class="swiper-box" :current="swiperCurrent" @transition="transition"
-				@animationfinish="animationfinish">
-				<swiper-item class="swiper-item">
-					<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="reachBottom">
-						<view class="page-box">
-							<view class="order" v-for="(res, index) in orderList[0]" :key="res.id">
-								<view class="top">
-								</view>
-							</view>
-							<u-loadmore :status="loadStatus[0]" bgColor="#f2f2f2"></u-loadmore>
-						</view>
-					</scroll-view>
-				</swiper-item>
-				<swiper-item class="swiper-item">
-					<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="reachBottom">
-						<view class="page-box">
-							<view class="order" v-for="(res, index) in  orderList[1]" :key="res.id">
-								<view class="top">
+		</u-navbar>
+		
+		<view class="u-demo">
+			<view class="u-demo-wrap" style="padding-left:0;padding-right:0;margin-left: -20rpx;margin-right: -5rpx;">
+				<view class="u-demo-area">
+						<u-cell-item center :is-link="true" :label="label" value="" index="index" @click="click" 
+						 :arrow="arrow" title="高数A" icon="list-dot">
+							<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+							<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+						</u-cell-item>
+						<u-cell-item center :is-link="true" :label="label" value="" index="index" @click="click" 
+						 :arrow="arrow" title="高数B" icon="list-dot">
+							<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+							<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+						</u-cell-item>
+						<u-cell-item center :is-link="true" :label="label" value="" index="index" @click="click" 
+						 :arrow="arrow" title="高数C" icon="list-dot">
+							<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+							<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+						</u-cell-item>
+						<u-cell-item center :is-link="true" :label="label" value="" index="index" @click="click"
+						 :arrow="arrow" title="高数D" icon="list-dot">
+							<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+							<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+						</u-cell-item>
 
-								</view>
-							</view>
-							<u-loadmore :status="loadStatus[1]" bgColor="#f2f2f2"></u-loadmore>
-						</view>
-					</scroll-view>
-				</swiper-item>
-			</swiper>
-		</view>
-		<u-tabbar :list="tabbar"></u-tabbar>
-	</view>
+				</view>
+			</view>
+			</view>
+			<u-tabbar :list="tabbar" :mid-button="false"></u-tabbar>
+			</view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				list: [{
-					cate_name: '我创建的',
-					cate_count: 4
-				}, {
-					cate_name: '我加入的'
-				}],
-				orderList: [
-					[1],
-					[1],
-					[1],
-					[1]
-				],
-				dataList: [{
-						id: 1,
-						store: '夏日流星限定贩卖',
-						deal: '交易成功',
-						goodsList: [{
-								goodsUrl: '//img13.360buyimg.com/n7/jfs/t1/103005/7/17719/314825/5e8c19faEb7eed50d/5b81ae4b2f7f3bb7.jpg',
-								title: '【冬日限定】现货 原创jk制服女2020冬装新款小清新宽松软糯毛衣外套女开衫短款百搭日系甜美风',
-								type: '灰色;M',
-								deliveryTime: '付款后30天内发货',
-								price: '348.58',
-								number: 2
-							},
-							{
-								goodsUrl: '//img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg',
-								title: '【葡萄藤】现货 小清新学院风制服格裙百褶裙女短款百搭日系甜美风原创jk制服女2020新款',
-								type: '45cm;S',
-								deliveryTime: '付款后30天内发货',
-								price: '135.00',
-								number: 1
-							}
-						]
-					},
-					{
-						id: 2,
-						store: '江南皮革厂',
-						deal: '交易失败',
-						goodsList: [{
-							goodsUrl: '//img14.360buyimg.com/n7/jfs/t1/60319/15/6105/406802/5d43f68aE9f00db8c/0affb7ac46c345e2.jpg',
-							title: '【冬日限定】现货 原创jk制服女2020冬装新款小清新宽松软糯毛衣外套女开衫短款百搭日系甜美风',
-							type: '粉色;M',
-							deliveryTime: '付款后7天内发货',
-							price: '128.05',
-							number: 1
-						}]
-					},
-					{
-						id: 3,
-						store: '三星旗舰店',
-						deal: '交易失败',
-						goodsList: [{
-								goodsUrl: '//img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg',
-								title: '三星（SAMSUNG）京品家电 UA65RUF70AJXXZ 65英寸4K超高清 HDR 京东微联 智能语音 教育资源液晶电视机',
-								type: '4K，广色域',
-								deliveryTime: '保质5年',
-								price: '1998',
-								number: 3
-							},
-							{
-								goodsUrl: '//img14.360buyimg.com/n7/jfs/t6007/205/4099529191/294869/ae4e6d4f/595dcf19Ndce3227d.jpg!q90.jpg',
-								title: '美的(Midea)639升 对开门冰箱 19分钟急速净味 一级能效冷藏双开门杀菌智能家用双变频节能 BCD-639WKPZM(E)',
-								type: '容量大，速冻',
-								deliveryTime: '保质5年',
-								price: '2354',
-								number: 1
-							}
-						]
-					},
-					{
-						id: 4,
-						store: '三星旗舰店',
-						deal: '交易失败',
-						goodsList: [{
-								goodsUrl: '//img10.360buyimg.com/n7/jfs/t22300/31/1505958241/171936/9e201a89/5b2b12ffNe6dbb594.jpg!q90.jpg',
-								title: '法国进口红酒 拉菲（LAFITE）传奇波尔多干红葡萄酒750ml*6整箱装',
-								type: '4K，广色域',
-								deliveryTime: '珍藏10年好酒',
-								price: '1543',
-								number: 3
-							},
-							{
-								goodsUrl: '//img10.360buyimg.com/n7/jfs/t1/107598/17/3766/525060/5e143aacE9a94d43c/03573ae60b8bf0ee.jpg',
-								title: '蓝妹（BLUE GIRL）酷爽啤酒 清啤 原装进口啤酒 罐装 500ml*9听 整箱装',
-								type: '一打',
-								deliveryTime: '口感好',
-								price: '120',
-								number: 1
-							}
-						]
-					},
-					{
-						id: 5,
-						store: '三星旗舰店',
-						deal: '交易成功',
-						goodsList: [{
-							goodsUrl: '//img12.360buyimg.com/n7/jfs/t1/52408/35/3554/78293/5d12e9cfEfd118ba1/ba5995e62cbd747f.jpg!q90.jpg',
-							title: '企业微信 中控人脸指纹识别考勤机刷脸机 无线签到异地多店打卡机WX108',
-							type: '识别效率高',
-							deliveryTime: '使用方便',
-							price: '451',
-							number: 9
-						}]
-					}
-				],
-				current: 0,
-				swiperCurrent: 0,
-				tabsHeight: 0,
-				dx: 0,
-				loadStatus: ['loadmore', 'loadmore', 'loadmore', 'loadmore'],
-			};
+				title: '我创建的',
+				backText: '返回',
+				backIconName: 'nav-back',
+				right: false,
+				showAction: false,
+				rightSlot: false,
+				useSlot: false,
+				background: {
+					'background-image': 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'
+				},
+				isBack: false,
+				search: false,
+				custom: false,
+				isFixed: true,
+				keyword: '',
+				// #ifdef MP
+				slotRight: false,
+				// #endif
+				// #ifndef MP
+				slotRight: true,
+				// #endificon: 'setting',
+				arrow: true,
+				label: '签到  提问 回复',
+				rightSlot: true,
+				checked: false
+			}
 		},
 		onLoad() {
 			this.tabbar = [{
-						iconPath: "home",
-						selectedIconPath: "home-fill",
-						text: '班课',
-						count: 0,
+							iconPath: "home",
+							selectedIconPath: "home-fill",
+							text: '班课',
+							count: 2,
 						//	isDot: true,
-						customIcon: false,
-						pagePath: "/pages/index/class"
-					},
-					{
-						iconPath: "photo",
-						selectedIconPath: "photo-fill",
-						text: '发现',
-						customIcon: false,
-						pagePath: "/pages/index/find"
-					},
-					{
-						iconPath: "photo",
-						selectedIconPath: "photo-fill",
-						text: '我的',
-						customIcon: false,
-						pagePath: "/pages/index/mine"
-					},
-				],
-				this.getOrderList(0);
-			this.getOrderList(1);
-			this.getOrderList(3);
-		},
+							customIcon: false,
+							pagePath: "/pages/index/class"
+						},
+						{
+							iconPath: "photo",
+							selectedIconPath: "photo-fill",
+							text: '发现',
+							customIcon: false,
+							pagePath: "/pages/index/find"
+						},
+						{
+							iconPath: "photo",
+							selectedIconPath: "photo-fill",
+							text: '我的',
+							customIcon: false,
+							pagePath: "/pages/index/mine"
+						},
+					]
+				},
 		computed: {
-			// 价格小数
-			priceDecimal() {
-				return val => {
-					if (val !== parseInt(val)) return val.slice(-2);
-					else return '00';
-				};
-			},
-			// 价格整数
-			priceInt() {
-				return val => {
-					if (val !== parseInt(val)) return val.split('.')[0];
-					else return val;
-				};
+			slotRightCurrent() {
+				return this.slotRight ? 0 : 1;
 			}
 		},
 		methods: {
-			reachBottom() {
-				// 此tab为空数据
-				if (this.current != 2) {
-					this.loadStatus.splice(this.current, 1, "loading")
-					setTimeout(() => {
-						this.getOrderList(this.current);
-					}, 1200);
+			titleChange(index) {
+				this.useSlot = false;
+				this.title = index == 0 ? '新闻' : index == 1 ? '新闻列表' : '雨打梨花深闭门，忘了青春，误了青春';
+			},
+			leftChange(index) {
+				if(index == 0) {
+					this.backText = '';
+					this.backIconName = 'arrow-leftward';
+				} else {
+					this.backText = '返回';
+					this.backIconName = 'arrow-left';
 				}
 			},
-			// 页面数据
-			getOrderList(idx) {
-				for (let i = 0; i < 5; i++) {
-					let index = this.$u.random(0, this.dataList.length - 1);
-					let data = JSON.parse(JSON.stringify(this.dataList[index]));
-					data.id = this.$u.guid();
-					this.orderList[idx].push(data);
+			searchChange(index) {
+				this.title = null;
+				this.useSlot = true;
+				this.search = false;
+				this.custom = false;
+				if(index == 0) {
+					this.title = '新闻';
+					this.useSlot = false;
+					this.rightSlot = false;
+				} else if(index == 1) {
+					this.showAction = false;
+					this.useSlot = true;
+					this.rightSlot = false;
+					this.search = true;
+					this.slotRight = false;
+				} else if(index == 2) {
+					this.useSlot = true;
+					this.showAction = true;
+					this.rightSlot = false;
+					this.search = true;
+					this.slotRight = false;
+				} else {
+					this.useSlot = true;
+					this.search = true;
+					this.showAction = false;
+					this.rightSlot = true;
+					this.slotRight = false;
 				}
-				this.loadStatus.splice(this.current, 1, "loadmore")
 			},
-			// 总价
-			totalPrice(item) {
-				let price = 0;
-				item.map(val => {
-					price += parseFloat(val.price);
-				});
-				return price.toFixed(2);
+			backChange(index) {
+				this.isBack = !!index;
 			},
-			// 总件数
-			totalNum(item) {
-				let num = 0;
-				item.map(val => {
-					num += val.number;
-				});
-				return num;
-			},
-			// tab栏切换
-			change(index) {
-				this.swiperCurrent = index;
-				this.getOrderList(index);
-			},
-			transition({
-				detail: {
-					dx
+			bgColorChange(index) {
+				this.background = {};
+				if(index == 0) {
+					this.background = {
+						'background-image': 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'
+					}
+				} else {
+					let color = index == 1 ? '#39CCCC' : index == 2 ? '#B471CC' : '#001f3f';
+					this.background = {
+						background: color
+					}
 				}
-			}) {
-				this.$refs.tabs.setDx(dx);
+				
 			},
-			animationfinish({
-				detail: {
-					current
+			rightChange(index) {
+				if(index == 0) {
+					this.slotRight = true;
+					this.useSlot = false;
+				} else {
+					this.slotRight = false;
 				}
-			}) {
-				this.$refs.tabs.setFinishCurrent(current);
-				this.swiperCurrent = current;
-				this.current = current;
+			},
+			customChange(index) {
+				this.search = false;
+				this.rightSlot = false;
+				if(index == 0) {
+					this.custom = true;
+					this.title = null;
+					this.isBack = false;
+					this.useSlot = true;
+				} else {
+					this.useSlot = false;
+					this.title = '新闻';
+					this.isBack = true;
+				}
+			},
+			click(){
+				uni.navigateTo({
+					url:"/pages/class/create"
+				})
+			},
+			newcreate(){
+				uni.navigateTo({
+					url:"/pages/class/create"
+				})
 			}
 		}
-	};
+	}
 </script>
 
 <style lang="scss" scoped>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.u-demo {
+		//height: 200vh;
+		height: calc(100% - 44px);
+		height: calc(100% - 44px - constant(safe-area-inset-top));
+		height: calc(100% - 44px - env(safe-area-inset-top));
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-
-	.order {
-		width: 710rpx;
-		background-color: #ffffff;
-		margin: 20rpx auto;
-		border-radius: 20rpx;
-		box-sizing: border-box;
-		padding: 20rpx;
-		font-size: 28rpx;
-
-		.top {
-			display: flex;
-			justify-content: space-between;
-
-			.left {
-				display: flex;
-				align-items: center;
-
-				.store {
-					margin: 0 10rpx;
-					font-size: 32rpx;
-					font-weight: bold;
-				}
-			}
-
-			.right {
-				color: $u-type-warning-dark;
-			}
-		}
-
-		.item {
-			display: flex;
-			margin: 20rpx 0 0;
-
-			.left {
-				margin-right: 20rpx;
-
-				image {
-					width: 200rpx;
-					height: 200rpx;
-					border-radius: 10rpx;
-				}
-			}
-
-			.content {
-				.title {
-					font-size: 28rpx;
-					line-height: 50rpx;
-				}
-
-				.type {
-					margin: 10rpx 0;
-					font-size: 24rpx;
-					color: $u-tips-color;
-				}
-
-				.delivery-time {
-					color: #e5d001;
-					font-size: 24rpx;
-				}
-			}
-
-			.right {
-				margin-left: 10rpx;
-				padding-top: 20rpx;
-				text-align: right;
-
-				.decimal {
-					font-size: 24rpx;
-					margin-top: 4rpx;
-				}
-
-				.number {
-					color: $u-tips-color;
-					font-size: 24rpx;
-				}
-			}
-		}
-
-		.total {
-			margin-top: 20rpx;
-			text-align: right;
-			font-size: 24rpx;
-
-			.total-price {
-				font-size: 32rpx;
-			}
-		}
-
-		.bottom {
-			display: flex;
-			margin-top: 40rpx;
-			padding: 0 10rpx;
-			justify-content: space-between;
-			align-items: center;
-
-			.btn {
-				line-height: 52rpx;
-				width: 160rpx;
-				border-radius: 26rpx;
-				border: 2rpx solid $u-border-color;
-				font-size: 26rpx;
-				text-align: center;
-				color: $u-type-info-dark;
-			}
-
-			.evaluate {
-				color: $u-type-warning-dark;
-				border-color: $u-type-warning-dark;
-			}
-		}
-	}
-
-	.centre {
-		text-align: center;
-		margin: 200rpx auto;
-		font-size: 32rpx;
-
-		image {
-			width: 164rpx;
-			height: 164rpx;
-			border-radius: 50%;
-			margin-bottom: 20rpx;
-		}
-
-		.tips {
-			font-size: 24rpx;
-			color: #999999;
-			margin-top: 20rpx;
-		}
-
-		.btn {
-			margin: 80rpx auto;
-			width: 200rpx;
-			border-radius: 32rpx;
-			line-height: 64rpx;
-			color: #ffffff;
-			font-size: 26rpx;
-			background: linear-gradient(270deg, rgba(249, 116, 90, 1) 0%, rgba(255, 158, 1, 1) 100%);
-		}
-	}
-
+	
 	.wrap {
-		display: flex;
-		flex-direction: column;
-		height: calc(100vh - var(--window-top));
-		width: 100%;
+		padding: 24rpx;
 	}
-
-	.swiper-box {
+	
+	.navbar-right {
+		margin-right: 24rpx;
+		display: flex;
+	}
+	
+	.search-wrap {
+		margin: 0 20rpx;
 		flex: 1;
 	}
-
-	.swiper-item {
-		height: 100%;
+	
+	.right-item {
+		margin: 0 12rpx;
+		position: relative;
+		color: #ffffff;
+		display: flex;
+	}
+	
+	.message-box {
+		
+	}
+	
+	.slot-wrap {
+		display: flex;
+		align-items: center;
+		flex: 1;
+	}
+	
+	.map-wrap {
+		display: flex;
+		align-items: center;
+		padding: 4px 6px;
+		background-color: rgba(240,240, 240, 0.35);
+		color: #fff;
+		font-size: 22rpx;
+		border-radius: 100rpx;
+		margin-left: 30rpx;
+	}
+	
+	.map-wrap-text {
+		padding: 0 6rpx;
 	}
 </style>
+
