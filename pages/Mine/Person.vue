960ea8d1c545eb5d1fefa674200ccfd6f8dc5a1e
Bigrice-bit <71315075+Bigrice-bit@users.noreply.github.com>
2021-05-09 11:11:42 +0800

    个人资料

--- pages/Mine/Person.vue
+++ pages/Mine/Person.vue
@@ -1,5 +1,8 @@
 <template>
 	<view>
+		<u-navbar title-color="#000000" back-icon-color="#000000" :is-fixed="isFixed" :is-back="isBack"
+			:background="background" :back-text-style="{color: '#fff'}" :title="title" :back-icon-name="backIconName"
+			:back-text="backText" @click="newcreate"> </u-navbar>
 		<view class="list-content">
 			<view class="list">
 				<view class="li " >
@@ -8,30 +11,38 @@
 					</view>
 					<view class="text">
 						<view class="phone-number">陈泓敏</view>
-						<view class="phone-number">18909XXXX67</view></view>
+						<view class="phone-number">18909XXXX67</view>
 					<image class="to" src="../../static/user/to.png"></image>
 				</view>
 			</view>
-			<view class="list">
+			<!-- <view class="list">
 				<view class="li " >
 					<view class="text">姓名</view>
-					<image class="to" src="../../static/user/to.png"></image>
+					<view class="to" >后端返回</view>
 				</view>
 				<view class="li " >
 					<view class="text">账号</view>
-					<image class="to" src="../../static/user/to.png"></image>
+					<view class="to" >后端返回</view>
+				</view>
+				<view class="li " >
+					<view class="text">出生年份</view>
+					<view class="to" >后端返回</view>
 				</view>
 				<view class="li " >
 					<view class="text">选择性别</view>
-					<image class="to" src="../../static/user/to.png"></image>
+					<u-radio-group :shape="shape" :size="size" :width="width" :wrap="wrap" v-model="value" @change="radioGroupChange" :activeColor="activeColor">
+						<u-radio @change="radioChange" v-for="(item, index) in list" :disabled="item.disabled"
+							:key="index" :name="item.name"
+						>{{item.name}}</u-radio>
+					</u-radio-group>
 				</view>
 				<view class="li " >
 					<view class="text">你所在的学校、院系及专业</view>
+					<view>本人的学校院系专业</view>
 					<image class="to" src="../../static/user/to.png"></image>
 				</view>
 				<view class="li " >
 					<view class="text">选择身份</view>
-					<image class="to" src="../../static/user/to.png"></image>
 				</view>
 				<view class="li " >
 					<view class="text">我是老师</view>
@@ -48,10 +59,59 @@
 				<view class="li " >
 					<view class="text">学号/工号</view>
 					<image class="to" src="../../static/user/to.png"></image>
-				</view>
+				</view> -->
+				<view class="list"></view>
+				<u-cell-group class="">
+					<u-cell-item center :is-link="true"  value="后端返回" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="姓名" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+					<u-cell-item center :is-link="true"  value="后端返回" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="账号" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+					<u-cell-item center :is-link="true"  value="后端返回" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="出生年份" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+					<u-cell-item center :is-link="true"  value="" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="选择性别" >
+					 <u-radio-group :shape="shape" :size="size" :width="width" :wrap="wrap" v-model="value" @change="radioGroupChange" :activeColor="activeColor">
+					 	<u-radio @change="radioChange" v-for="(item, index) in list" :disabled="item.disabled"
+					 		:key="index" :name="item.name"
+					 	>{{item.name}}</u-radio>
+					 </u-radio-group>
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+					<u-cell-item center :is-link="true" :label="label" value="" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="arrow" title="你所在的学校、院系及专业(索引列表)" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+					<u-cell-item center :is-link="true"  value="" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="选择身份" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+						<u-radio-group :shape="shape" :size="size" :width="width" :wrap="wrap" v-model="value" @change="radioGroupChange" :activeColor="activeColor">
+							<u-radio @change="radioChange" v-for="(item, index) in identity" :disabled="item.disabled"
+								:key="index" :name="item.name"
+							>{{item.name}}</u-radio>
+						</u-radio-group>
+					</u-cell-item>
+					<u-cell-item center :is-link="true"  value="后端返回" i ndex="index" @click="click" :hover-class="hoverClass"
+					 :arrow="false" title="学号/工号" >
+						<u-badge :absolute="false" v-if="rightSlot == 'badge'" count="105" slot="right-icon"></u-badge>
+						<u-switch v-if="rightSlot == 'switch'" slot="right-icon" v-model="checked"></u-switch>
+					</u-cell-item>
+				</u-cell-group>
+				
 			</view>
 		</view>
-		<button type="error" style="margin-top: 200rpx;">保存</button>
+		<button type="error" class="button">保存</button>
 	</view>
 </template>
 
@@ -59,16 +119,81 @@
 	export default {
 		data() {
 			return {
+				title:'用户信息',
+				backText: '返回',
+				backIconName: 'nav-back',
+				isBack: true,
+				search: false,
+				custom: false,
+				isFixed: true,
+				background: {
+					'background-image': 'linear-gradient(45deg, rgb(255, 255, 255), rgb(255, 255, 255))'
+				},
+				list: [
+								{
+									name: '男',
+									disabled: false
+								},
+								{
+									name: '女',
+									disabled: false
+								}
+							],
+				identity:[
+								{
+									name: '我是老师',
+									disabled: false
+								},
+								{
+									name: '我是学生',
+									disabled: false
+								},
+								{
+									name: '其他',
+									disabled: false
+								}
+							],
+							// u-radio-group的v-model绑定的值如果设置为某个radio的name，就会被默认选中
+				disabled: false,
+				result: '荔枝',
+				shape: 'circle', 
+				value: '荔枝',
+				activeColor: '#2979ff',
+				size: 34,
+				wrap: false,
+				width: 'auto',
+				icon: 'setting',
+				arrow: true,
+				label: '后端返回学校等',
+				rightSlot: true,
+				checked: false
 			};
 		},
 		onLoad() {
 		},
+		computed: {
+			hoverClass() {
+				// 如果右侧是switch步进器组件的话，去掉cell的点击反馈，因为这个时候点击的反馈应该在switch上
+				return this.rightSlot == 'switch' ? 'none' : 'u-cell-hover';
+			}
+		},
 		methods: {
+			// 选中某个单选框时，由radio时触发
+					radioChange(e) {
+						console.log(e);
+					},
+					// 选中任一radio时，由radio-group触发
+					radioGroupChange(e) {
+						// console.log(e);
+					},
 			Person(){
 				uni.navigateTo({
 						url: './Person'
 				});
 			},
+			click(index) {
+				// console.log(index);
+			}
 		}
 	}
 </script>
@@ -103,6 +228,16 @@ page{
 		width: 100%
 	}
 	
+	.button{
+		// margin-top: 100rpx;
+		// bottom: var(--window-bottom);
+		// position: fixed;  
+		bottom: -80rpx;
+	}
+	
+	.radio{
+		
+	}
 	
 .box{
 	width: 650upx;
@@ -175,13 +310,13 @@ page{
 }
 .list{
 	width:100%;
-	border-bottom:15upx solid  #f1f1f1;
+	border-bottom:25upx solid  #f1f1f1;
 	background: #fff;
 	&:last-child{
 		border: none;
 	}
 	.li{
-		width:100%;
+		width:92%;
 		height:100upx;
 		padding:0 4%;
 		border-bottom:1px solid rgb(243,243,243);
@@ -189,6 +324,15 @@ page{
 		align-items:center;
 	&.noborder{
 		border-bottom:0
+		}
+		.icon{
+			flex-shrink:0;
+			width:50upx;
+			height:50upx;
+			image{
+				width:50upx;
+				height:50upx;
+			}
 		}
 		.text{
 			padding-left:20upx;
