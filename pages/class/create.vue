054deceb6c1d8e8f1bc3a952d32fa07ac4cde911
Bigrice-bit <1151072975@qq.com>
2021-06-07 23:30:55 +0800

    创建班课获取学校接口

--- pages/class/create.vue
+++ pages/class/create.vue
@@ -22,7 +22,7 @@
 					<u-input v-model="form.class" placeholder="请输入班级名称" />
 				</u-form-item>
 				<u-form-item :label-position="labelPosition" label="课程" prop="team" label-width="150">
-					<u-input v-model="form.classname" placeholder="请输入课程名称" />
+					<u-input v-model="form.course" placeholder="请输入课程名称" />
 				</u-form-item>
 
 				<u-form-item :label-position="labelPosition" label="学期" prop="team" label-width="150">
@@ -38,7 +38,8 @@
 				<u-button type="default" :ripple="true" shape="circle" class="loginBtn" @click="submit">创建</u-button>
 				<u-select mode="single-column" :list="selectList" v-model="selectShow" @confirm="selectConfirm">
 				</u-select>
-				<u-select mode="mutil-column-auto" :list="SchoolList" v-model="pickerShow" @confirm="selectconfirm" @cancel="cancel">
+				<u-select mode="mutil-column-auto" :list="SchoolList" v-model="pickerShow" @confirm="selectconfirm"
+					@cancel="cancel">
 				</u-select>
 
 			</view>
@@ -67,7 +68,7 @@
 				}],
 				form: {
 					class: '',
-					classname: '',
+					course: '',
 					team: '',
 					school: '',
 				},
@@ -84,8 +85,11 @@
 					'background-image': 'linear-gradient(45deg, rgb(255, 255, 255), rgb(255, 255, 255))'
 				},
 				data: {
-					'courseName': '',
 					'creator': '',
+					'courseName': '',
+					'ClassCourseName': '',
+					'Term': '',
+					'CollegePointId': '',
 				},
 				params: {
 					year: true,
@@ -114,169 +118,241 @@
 					{
 						value: '2021-2022下学期',
 						label: '2021-2022下学期'
+					},
+					{
+						value: '2022-2023上学期',
+						label: '2022-2023上学期'
+					},
+					{
+						value: '2022-2023下学期',
+						label: '2022-2023下学期'
 					}
 				],
 				SchoolList: [{
-						value: 1,
-						label: '中国',
-						children: [{
-								value: 2,
-								label: '广东',
-								children: [{
-										value: 3,
-										label: '深圳'
-									},
-									{
-										value: 4,
-										label: '广州'
-									}
-								]
-							},
-							{
-								value: 5,
-								label: '广西',
-								children: [{
-										value: 6,
-										label: '南宁'
-									},
-									{
-										value: 7,
-										label: '桂林'
-									}
-								]
-							}
-						]
-					},
-					{
-						value: 8,
-						label: '美国',
-						children: [{
-							value: 9,
-							label: '纽约',
-							children: [{
-								value: 10,
-								label: '皇后街区'
-							}]
-						}]
-					}
-				]
-		};
-	},
-	computed: {
+					value: null,
+					label: null,
+					children: [{
+						value: null,
+						label: null,
+						children: [{}]
+					}]
+				}]
+			};
+		},
+		computed: {
 			status() {
 				return this.show == true ? 0 : 1;
 			}
 		},
-	methods: {
-		// 选择地区回调
-		regionConfirm(e) {
-			this.form.school = e.province.label + '-' + e.city.label + '-' + e.area.label;
-		},
-		statusChange(index) {
-			this.show = index == 0 ? true : false;
-		},
-		modeChange(index) {
-			this.mode = ['selector', 'multiSelector', 'time', 'region'][index];
-			if (this.mode == 'selector') {
-				this.range = ['一', '片', '冰', '心', '在', '玉', '壶'];
-				this.defaultSelector = [0];
-			}
-			if (this.mode == 'multiSelector') {
-				this.range = [
-					['亚洲', '欧洲'],
-					['中国', '日本'],
-					['北京', '上海', '广州']
-				];
-				this.defaultSelector = [0, 0, 0];
-			}
-			this.show = true;
-		},
-		defaultTimeChange(index) {
-			this.defaultTime = index == 0 ? '2019-12-11 20:15:35' : '2020-02-05 13:09:42';
-			this.mode = 'time';
-			this.show = true;
-		},
-		defaultRegionChange(index) {
-			this.defaultRegion = index == 0 ? ['广东省', '深圳市', '宝安区'] : ['海南省', '三亚市', '海棠区'];
-			this.mode = 'region';
-			this.show = true;
-		},
-		minSecChange(index) {
-			if (index == 0) {
-				this.params.hour = true;
-				this.params.minute = true;
-				this.params.second = true;
-			}
-			if (index == 1) {
-				this.params.hour = false;
-				this.params.minute = false;
-				this.params.second = false;
-			}
-			this.mode = 'time';
-			this.show = true;
-		},
-		cancel(e) {
-			console.log(e);
-		},
-		click() {
-			this.show = true;
-		},
-		submit() {
-			// uni.getStorage({
-			// 	key: "LoginKey",
-			// 	success(e) {
-			// 		e.data //这就是你想要取的token
-			// 		console.log(e.data);
-			// 	}
-			// });
-			// let LoginKey = uni.getStorage('LoginKey');
-			try {
-				const value = uni.getStorageSync("LoginKey");
-				if (value) {
-					console.log(value);
-					this.data.creator = value;
-				}
-			} catch (e) {
-				console.log(e);
-			}
-			console.log(this.data.creator);
-			this.data.courseName = this.form.classname;
-			// this.data.creator = 47;
-			console.log(this.data);
-			this.$Api.addCourses(this.data).then(res => {
-				if (res.data.success) {
-					let kins = res.data.data.courseId;
-					uni.setStorage({
-						key: this.data.courseName,
-						data: kins,
-						success: function() {
-							setTimeout(function() {
-								uni.navigateTo({
-									url: "/pages/class/success-create"
+		onLoad() {
+			this.$Api.getCollege().then(res => {
+								// console.log(res)
+								if (res.data.success) {
+									console.log(res.data.success)
+									let server = res.data.data;
+									// let values = server.typeList
+									for (var i = 0; i < server.length; i++) {
+										var obj = {
+											label: server[i].collegePoint.collegePointName,
+											value: server[i].collegePoint.collegePointId,
+										};
+										this.SchoolList.push(obj);
+									}
+									for (var i = 0; i < server.length; i++) {
+										for (var j = 0; k < server[i].collegePoints.length; k++) {
+											var obj = {
+												label: server[i].collegePoints[j].collegePointName,
+												value: server[i].collegePoints[j].collegePointId,
+											};
+											this.SchoolList.children.push(obj)
+										}
+									}
+									for (var i = 0; i < server.length; i++) {
+										for (var j = 0; j < server[i].collegePoints.length; j++) {
+											var obj = {
+												label: server[i].collegePoints[j].collegePointName,
+												value: server[i].collegePoints[j].collegePointId,
+											};
+											this.SchoolList.children.push(obj)
+										}
+									}
+									for (var i = 0; i < server.length; i++) {
+										for (var j = 0; j < server[i].collegePoints.length; j++) {
+											for (var k = 0; k < server[i].collegePoints[j].length; k++) {
+												var obj = {
+													label: server[i].collegePoints[j].collegePoints[k].collegePointName,
+													value: server[i].collegePoints[j].collegePoints[k].collegePointId,
+												};
+												this.SchoolList.children.children.push(obj)
+											}
+										}
+									}
+							} else {
+								uni.showToast({
+									icon: "none",
+									title: '暂无数据'
 								})
-							}, 1000);
-							// uni.switchTab({
-							// 	url: '/pages/index/class'
-							// });
-						}
+							}
 					})
-
+		},
+		methods: {
+			// 选择地区回调
+			regionConfirm(e) {
+				this.form.school = e.province.label + '-' + e.city.label + '-' + e.area.label;
+			},
+			statusChange(index) {
+				this.show = index == 0 ? true : false;
+			},
+			modeChange(index) {
+				this.mode = ['selector', 'multiSelector', 'time', 'region'][index];
+				if (this.mode == 'selector') {
+					this.range = ['一', '片', '冰', '心', '在', '玉', '壶'];
+					this.defaultSelector = [0];
 				}
-			})
-		},
-		selectconfirm(e) {
-			this.form.school = '';
-			e.map((val, index) => {
-				this.form.school += this.form.school == '' ? val.label : '-' + val.label;
-			})
-		},
-		Show(){
-			this.pickerShow = true;
-			this.$Api.getCollege().then(res=>
-			{
-				console.log(res);
-			})
-		}
+				if (this.mode == 'multiSelector') {
+					this.range = [
+						['亚洲', '欧洲'],
+						['中国', '日本'],
+						['北京', '上海', '广州']
+					];
+					this.defaultSelector = [0, 0, 0];
+				}
+				this.show = true;
+			},
+			defaultTimeChange(index) {
+				this.defaultTime = index == 0 ? '2019-12-11 20:15:35' : '2020-02-05 13:09:42';
+				this.mode = 'time';
+				this.show = true;
+			},
+			defaultRegionChange(index) {
+				this.defaultRegion = index == 0 ? ['广东省', '深圳市', '宝安区'] : ['海南省', '三亚市', '海棠区'];
+				this.mode = 'region';
+				this.show = true;
+			},
+			minSecChange(index) {
+				if (index == 0) {
+					this.params.hour = true;
+					this.params.minute = true;
+					this.params.second = true;
+				}
+				if (index == 1) {
+					this.params.hour = false;
+					this.params.minute = false;
+					this.params.second = false;
+				}
+				this.mode = 'time';
+				this.show = true;
+			},
+			cancel(e) {
+				console.log(e);
+			},
+			click() {
+				this.show = true;
+			},
+			submit() {
+				// uni.getStorage({
+				// 	key: "LoginKey",
+				// 	success(e) {
+				// 		e.data //这就是你想要取的token
+				// 		console.log(e.data);
+				// 	}
+				// });
+				// let LoginKey = uni.getStorage('LoginKey');
+				try {
+					const value = uni.getStorageSync("LoginKey");
+					if (value) {
+						console.log(value);
+						this.data.creator = value;
+
+					}
+				} catch (e) {
+					console.log(e);
+				}
+				console.log(this.data.creator);
+				this.data.ClassCourseName = this.form.class;
+				this.data.course = this.form.classname;
+				// this.data.creator = 47;
+				console.log(this.data);
+				this.$Api.AddClass(this.data).then(res => {
+					if (res.data.success) {
+						let kins = res.data.data.courseId;
+						uni.setStorage({
+							key: this.data.courseName,
+							data: kins,
+							success: function() {
+								setTimeout(function() {
+									uni.navigateTo({
+										url: "/pages/class/success-create"
+									})
+								}, 1000);
+								// uni.switchTab({
+								// 	url: '/pages/index/class'
+								// });
+							}
+						})
+
+					}
+				})
+			},
+			selectconfirm(e) {
+				this.form.school = '';
+				e.map((val, index) => {
+					this.form.school += this.form.school == '' ? val.label : '-' + val.label;
+				})
+			},
+			Show() {
+				this.pickerShow = true;
+				console.log(this.SchoolList)
+	// 			this.$Api.getCollege().then(res => {
+	// 					// console.log(res)
+	// 					if (res.data.success) {
+	// 						console.log(res.data.success)
+	// 						let server = res.data;
+	// 						// let values = server.typeList
+	// 						for (var i = 0; i < server.length; i++) {
+	// 							var obj = {
+	// 								label: server[i].collegePoint.collegePointName,
+	// 								value: server[i].collegePoints.collegePointId,
+	// 							};
+	// 							this.SchoolList.push(obj);
+	// 						}
+	// 						for (var i = 0; i < server.length; i++) {
+	// 							for (var j = 0; k < server[i].collegePoints.length; k++) {
+	// 								var obj = {
+	// 									label: server[i].collegePoints[j].collegePointName,
+	// 									value: server[i].collegePoints[j].collegePointId,
+	// 								};
+	// 								this.SchoolList.children.push(obj)
+	// 							}
+	// 						}
+	// 						for (var i = 0; i < server.length; i++) {
+	// 							for (var j = 0; j < server[i].collegePoints.length; j++) {
+	// 								var obj = {
+	// 									label: server[i].collegePoints[j].collegePointName,
+	// 									value: server[i].collegePoints[j].collegePointId,
+	// 								};
+	// 								this.SchoolList.children.push(obj)
+	// 							}
+	// 						}
+	// 						for (var i = 0; i < server.length; i++) {
+	// 							for (var j = 0; j < server[i].collegePoints.length; j++) {
+	// 								for (var k = 0; k < server[i].collegePoints[j].length; k++) {
+	// 									var obj = {
+	// 										label: server[i].collegePoints[j].collegePoints[k].collegePointName,
+	// 										value: server[i].collegePoints[j].collegePoints[k].collegePointId,
+	// 									};
+	// 									this.SchoolList.children.children.push(obj)
+	// 								}
+	// 							}
+	// 						}
+	// 				} else {
+	// 					uni.showToast({
+	// 						icon: "none",
+	// 						title: '暂无数据'
+	// 					})
+	// 				}
+	// 		})
+	}
 	}
 
 	};
